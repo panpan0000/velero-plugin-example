c8c9135a28ec734ddd12eac6ee50bfaa7f3167be
Peter Pan <Peter.Pan@daocloud.io>
2023-06-21 04:43:48 -0400

    add Restore Plugin for Ingress v1beta1 to v1

--- internal/plugin/restoreplugin.go
+++ internal/plugin/restoreplugin.go
@@ -17,9 +17,20 @@ limitations under the License.
 package plugin
 
 import (
+	"github.com/pkg/errors"
+
 	"github.com/sirupsen/logrus"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
-	"k8s.io/apimachinery/pkg/api/meta"
+	//"k8s.io/apimachinery/pkg/api/meta"
+	//"k8s.io/apimachinery/pkg/util/yaml"
+	"gopkg.in/yaml.v2"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	networkingv1 "k8s.io/api/networking/v1"
+	networkingv1beta1 "k8s.io/api/networking/v1beta1"
+	//"encoding/json"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 // RestorePlugin is a restore item action plugin for Velero
@@ -38,27 +49,90 @@ func NewRestorePlugin(log logrus.FieldLogger) *RestorePlugin {
 // A RestoreItemAction's Execute function will only be invoked on items that match the returned
 // selector. A zero-valued ResourceSelector matches all resources.
 func (p *RestorePlugin) AppliesTo() (velero.ResourceSelector, error) {
-	return velero.ResourceSelector{}, nil
+	return velero.ResourceSelector{
+		IncludedResources: []string{"ingresses"},
+	}, nil
 }
 
 // Execute allows the RestorePlugin to perform arbitrary logic with the item being restored,
 // in this case, setting a custom annotation on the item being restored.
 func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
-	p.log.Info("Hello from my RestorePlugin!")
+	ingress := networkingv1beta1.Ingress{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(input.Item.UnstructuredContent(), &ingress); err != nil {
+				return nil, errors.WithStack(err)
+	}
+	p.log.Infof("Peter: ingress APIVersion=%s",ingress.APIVersion)
+	p.log.Infof("Peter: ingress all content=%s",ingress)
+	if ingress.APIVersion == "networking.k8s.io/v1beta1" {
+		ingressV1 := networkingv1.Ingress{}
+		//p.log.Infof("Peter: EMPTY ingress v1 -- =%s",ingressV1)
+		ingressV1.Kind = "Ingress"
+		ingressV1.APIVersion = "networking.k8s.io/v1"
+		//p.log.Infof("Peter:  After with KIND: ingress v1 -- =%s",ingressV1)
+		// 复制元数据信息
+		ingressV1.ObjectMeta.Name = ingress.ObjectMeta.Name
+		ingressV1.ObjectMeta.Namespace = ingress.ObjectMeta.Namespace
 
-	metadata, err := meta.Accessor(input.Item)
+		// 复制spec
+		ingressV1.Spec.Rules = make([]networkingv1.IngressRule, len(ingress.Spec.Rules))
+		for i, rule := range ingress.Spec.Rules {
+			ingressV1.Spec.Rules[i].Host = rule.Host
+			ingressV1.Spec.Rules[i].IngressRuleValue = networkingv1.IngressRuleValue{
+				HTTP: &networkingv1.HTTPIngressRuleValue{
+					Paths: make([]networkingv1.HTTPIngressPath, len(rule.HTTP.Paths)),
+				},
+			}
+			for j, path := range rule.HTTP.Paths {
+				ingressV1.Spec.Rules[i].IngressRuleValue.HTTP.Paths[j] = networkingv1.HTTPIngressPath{
+					Path: path.Path,
+					Backend: networkingv1.IngressBackend{
+						Service: &networkingv1.IngressServiceBackend{
+							Name: path.Backend.ServiceName,
+							Port: networkingv1.ServiceBackendPort{
+								Number: path.Backend.ServicePort.IntVal,
+							},
+						},
+					},
+				}
+			}
+		}
+		p.log.Infof("Peter: ingressV1 Kind=%s, APIVersion=%s",ingressV1.Kind, ingressV1.APIVersion)
+		/*if true{
+			outBytes, err := yaml.Marshal(ingressV1)
+			if err != nil {
+				panic(err.Error())
+			}
+			// 输出 YAML 格式的 Ingress v1数据
+			p.log.Infof("Peter The YAML is =%s",string(outBytes))
+		}*/
+
+		inputMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&ingressV1)
+		if err != nil {
+			return nil, errors.WithStack(err)
+
+		}
+		return velero.NewRestoreItemActionExecuteOutput(&unstructured.Unstructured{Object: inputMap}), nil
+
+
+	} else {
+		return velero.NewRestoreItemActionExecuteOutput(input.Item), nil 
+	}
+
+	/*
+	ObjectMeta, err := meta.Accessor(input.Item)
 	if err != nil {
 		return &velero.RestoreItemActionExecuteOutput{}, err
 	}
 
-	annotations := metadata.GetAnnotations()
+	annotations := ObjectMeta.GetAnnotations()
 	if annotations == nil {
 		annotations = make(map[string]string)
 	}
 
 	annotations["velero.io/my-restore-plugin"] = "1"
 
-	metadata.SetAnnotations(annotations)
+	ObjectMeta.SetAnnotations(annotations)
 
 	return velero.NewRestoreItemActionExecuteOutput(input.Item), nil
+	*/
 }
